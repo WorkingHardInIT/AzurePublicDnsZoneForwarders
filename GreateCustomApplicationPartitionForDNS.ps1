73e5cfdbdb968f526438e87b5f9047c952f7cedb
WorkingHardInIT <didier.van.hoye@hotmail.com>
2022-08-09 12:25:41 +0200

    Added PoSh to show how to create  your own custom DNS server directory partition(s)

---
+++ GreateCustomApplicationPartitionForDNS.ps1
@@ -0,0 +1,11 @@
+#This is just demo code to show you how to create your own custom DNS server directory partition(s)
+
+Add-DnsServerDirectoryPartition -Name "OP-BLUE-ADDS-SITE" -ComputerName 'DC01'
+Get-DnsServerDirectoryPartition -ComputerName 'DC01' |Format-Table -AutoSize
+Register-DnsServerDirectoryPartition -Name "OP-BLUE-ADDS-SITE" -ComputerName 'DC02'
+Get-DnsServerDirectoryPartition -Name "OP-BLUE-ADDS-SITE" -ComputerName 'DC02' | Format-Table -AutoSize
+
+Add-DnsServerDirectoryPartition -Name "OP-RED-ADDS-SITE" -ComputerName 'DC03'
+Get-DnsServerDirectoryPartition -ComputerName 'DC03' |Format-Table -AutoSize
+Register-DnsServerDirectoryPartition -Name "OP-RED-ADDS-SITE" -ComputerName 'DC04'
+Get-DnsServerDirectoryPartition -Name "OP-RED-ADDS-SITE" -ComputerName 'DC04' | Format-Table -AutoSize
